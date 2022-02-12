5b992b34090815d821164661319901bf102bf95a
JavaHammes <85684605+JavaHammes@users.noreply.github.com>
2022-02-12 11:47:23 +0100

    Update testWindow.c

--- testWindow.c
+++ testWindow.c
@@ -1,15 +1,37 @@
 // -lgdi32 -luser32 -lkernel32 -lcomctl32 -lm -mwindows
 #include <windows.h>
+#include <stdio.h>
 
 const char g_szClassName[] = "MainWindow";
 
+const UINT WINDOW_WIDTH = 900;
+const UINT WINDOW_HEIGHT = 450;
 
+const UINT HEIGHT_SCALE = 20;
+const UINT OFFSET_X = 10;
+
+int arr[] = {10,2,9,3,8,4,7,5,6,1};
+const UINT size = sizeof(arr)/sizeof(arr[0]);
+
+void paintRectangles(PAINTSTRUCT ps){
+	int i;
+	UINT posX;
+	UINT height;
+	
+
+	for ( i = 0; i < size; i++){
+		posX = 10 + OFFSET_X * i;
+		height = (WINDOW_HEIGHT - 50) - HEIGHT_SCALE * arr[i];
+		Rectangle(ps.hdc, posX, WINDOW_HEIGHT - 50, posX + OFFSET_X, height);
+		printf("index: %d\nheight: %d\n", i, height);
+	}	
+
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
     	HDC hdc;
 	RECT rcTmp;
 	PAINTSTRUCT ps;
-
 	
 	switch(msg){
 	case WM_CREATE:
@@ -19,7 +41,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 	case WM_PAINT:
 	{
 		BeginPaint(hwnd, &ps);
-		Rectangle(ps.hdc, 100, 100, 200, 200);
+
+		paintRectangles(ps);
 
 		EndPaint(hwnd, &ps);		
 	}
@@ -65,7 +88,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmpLine
 			g_szClassName,
 			"Test Window",
 			WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU,
-			CW_USEDEFAULT, CW_USEDEFAULT, 900, 450, 
+			CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, 
 			NULL, NULL, hInstance, NULL);
 
 	if(hwnd == NULL){
